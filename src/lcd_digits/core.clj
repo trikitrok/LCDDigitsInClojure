416be2a201b803f38a4a78b119745d2f831bc8cc
trikitrok <trikitrok@gmail.com>
2015-02-01 00:47:45 +0100

    Refactor: reduced duplication

--- src/lcd_digits/core.clj
+++ src/lcd_digits/core.clj
@@ -23,11 +23,19 @@
 (defn- digits-in [n]
   (map #(Integer/parseInt (str %)) (str n)))
 
+(def ^:private fns-by-row
+  {1 first-row
+   2 second-row
+   3 third-row})
+
+(defn- display-row [row-num n]
+  (str 
+    (clojure.string/join " " (map (fns-by-row row-num) 
+                                  (digits-in n)))
+       "\n"))
+
 (defn display [n]
   (str 
-    (clojure.string/join " " (map first-row (digits-in n)))
-    "\n"
-    (clojure.string/join " " (map second-row (digits-in n)))
-    "\n"
-    (clojure.string/join " " (map third-row (digits-in n)))
-    "\n"))
+    (display-row 1 n)
+    (display-row 2 n)
+    (display-row 3 n)))
