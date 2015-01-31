0200de60959829d8338f57f7bf76ff80234c2376
trikitrok <trikitrok@gmail.com>
2015-02-01 00:26:44 +0100

    Refactor: reduced duplication extracting \n from all helpers

--- src/lcd_digits/core.clj
+++ src/lcd_digits/core.clj
@@ -2,26 +2,29 @@
 
 (defn- first-row [n]
   (if (or (= n 1) (= n 4))
-    "   \n"
-    " _ \n"))
+    "   "
+    " _ "))
 
 (defn- second-row [n]
   (cond 
-    (or (= n 8) (= n 9) (= n 4)) "|_|\n"
-    (= n 0) "| |\n"
-    (or (= n 5) (= n 6)) "|_ \n"
-    (or (= n 2) (= n 3)) " _|\n"
-    :else "  |\n"))
+    (or (= n 8) (= n 9) (= n 4)) "|_|"
+    (= n 0) "| |"
+    (or (= n 5) (= n 6)) "|_ "
+    (or (= n 2) (= n 3)) " _|"
+    :else "  |"))
 
 (defn- third-row [n]
   (cond 
-    (or (= n 8) (= n 0) (= n 6)) "|_|\n"
-    (or (= n 5) (= n 3)) " _|\n"
-    (= n 2) "|_ \n"
-    :else "  |\n"))
+    (or (= n 8) (= n 0) (= n 6)) "|_|"
+    (or (= n 5) (= n 3)) " _|"
+    (= n 2) "|_ "
+    :else "  |"))
 
 (defn display [n]
   (str 
     (first-row n)
+    "\n"
     (second-row n)
-    (third-row n)))
+    "\n"
+    (third-row n)
+    "\n"))
